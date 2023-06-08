c65b6069d03bce97da29570bf6e864539871ce00
Marijus <markakk@gmail.com>
2023-06-08 17:44:05 +0300

    Convert weight to kg

--- classes/OmnivaIntHelper.php
+++ classes/OmnivaIntHelper.php
@@ -33,4 +33,26 @@ class OmnivaIntHelper
         }
         return null;
     }
+
+    public function changeWeightUnit($value, $current_unit, $new_unit)
+    {
+        $to_kg = array(
+            'mg' => 0.000001,
+            'g' => 0.001,
+            'kg' => 1,
+            't' => 1000,
+            'gr' => 0.0000648,
+            'k' => 0.0002,
+            'oz' => 0.02835,
+            'lb' => 0.45359,
+            'cnt' => 100,
+        );
+
+        if (isset($to_kg[$current_unit]) && isset($to_kg[$new_unit])) {
+            $current_kg = $value * $to_kg[$current_unit]; //Change value to kg
+            return $current_kg / $to_kg[$new_unit]; //Change kg value to new unit
+        }
+
+        return $value;
+    }
 }
